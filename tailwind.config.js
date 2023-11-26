00b04f3d0bb739a1e6fff55f703175e42ec707d1
baadillahnabil <baadillahnabil@gmail.com>
2023-11-27 03:22:30 +0800

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00B2FF',
+        secondary: '#FFC700',
+        background: '#F7F9FF',
+        text: '#28293D'
+      },
+    },
+  },
+};
